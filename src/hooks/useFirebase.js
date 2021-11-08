e76e7e3aa9dcdcd3ba3eeec1d485808075176be6
mahbubmanna <mahbubmanna@yahoo.com>
2021-11-08 09:58:27 +0600

    first commit

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,53 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import initalizeAuthentication from "../Pages/Login/Firebase/firebase.init";
+
+initalizeAuthentication();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    const auth = getAuth();
+
+    const signInUsingGoogle = () => {
+        setIsLoading(true);
+        const googleProvider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    //observe user state change
+    useEffect(() => {
+        const unSubscribed = onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            }
+            else {
+                setUser({});
+            }
+            setIsLoading(false);
+        });
+        return () => unSubscribed;
+    }, [])
+
+    const logOut = () => {
+        setIsLoading(true);
+        signOut(auth)
+            .then(() => { })
+            .finally(() => setIsLoading(false));
+    }
+
+    return {
+        user,
+        signInUsingGoogle,
+        logOut
+    }
+}
+
+
+export default useFirebase;
